c7169cb1652e33030559532709ff1133b0314a4a
Shahid Amin <schboostie@gmail.com>
2023-06-13 01:39:22 +0100

    practise file added

---
+++ 02-backroads-project/final/src/components/AboutSA.js
@@ -0,0 +1,16 @@
+import {pageLinks, socialLinks} from '../data'
+import PageLinks from './PageLinks'
+import SocialLink from './SocialLink'
+
+const Footer = () =>{
+  return(
+    <footer className="section footer">
+    <PageLinks parentClass='footer-links' itemClass='footer-link' />
+    <ul className='footer-icons'>
+      {socialLinks.map((link)=>{
+        return<SocialLink key={link.id}{...link}itemClass='footer-icon'/>
+      })}
+    </ul>
+    <p className='copyright'
+  )
+}
