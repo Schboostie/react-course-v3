2d489635d32f0036d564393aadcba9b313b1a257
Shahid Amin <schboostie@gmail.com>
2023-08-02 18:22:11 +0100

    =changes made

--- 00-Tutorial/amazon-book/src/Book.js
+++ 00-Tutorial/amazon-book/src/Book.js
@@ -6,9 +6,14 @@ const Book = (props) => {
       <img src={img} alt={title} />
       <h2>{title}</h2>
 
-      <h4>{author} </h4>
+      <h4>{author}</h4>
     </article>
   );
 };
 
 export default Book;
+
+/* I wanted to come aswell to 
+play with you both in hte park
+did you if we knew taht you wanted to cm
+we would have taken you to  the park*/
