4870270c33b2f4065746fbb9873c428498ffaf8d
Shahid Amin <schboostie@gmail.com>
2023-12-02 05:15:53 +0000

    added files
    done

--- 02-backroads-project/final/src/components/Title.js
+++ 02-backroads-project/final/src/components/Title.js
@@ -1,9 +1,10 @@
-const Title = ({ title, subTitle }) => {
+const Title = ({ title, subTitle, bitchingBox }) => {
   return (
-    <div className='section-title'>
+    <div className="section-title">
       <h2>
         {title} <span>{subTitle}</span>
       </h2>
+      <p>You</p>
     </div>
   );
 };
---
+++ 02-backroads-project/new-app/tutorial/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { pageLinks, socialLinks } from "../data";
+import logo from "../images/logo.svg";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="social-links" id="social-links">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
